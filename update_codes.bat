b435430d7e0ae5b78bce4e3dabed09fb110c2748
Zero Fanker <zcyfksn@vip.qq.com>
2020-02-05 22:14:32 +0800

    auto link yrpp with batch file

---
+++ update_codes.bat
@@ -0,0 +1,14 @@
+git init
+git remote add origin https://gitee.com/Zero_Fanker/Ares.git
+git fetch
+git checkout Verion0A
+mkdir lib
+cd lib
+mkdir YRPP
+cd YRPP
+git init
+git remote add origin https://gitee.com/Zero_Fanker/YRpp.git
+git fetch
+git checkout Ares0A
+cd ../..
+pause
